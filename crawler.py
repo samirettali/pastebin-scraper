dee91d4e4caa9f6c64307f2cdf4e80954f3fc781
Samir Ettali <ettali.samir@gmail.com>
2020-03-06 00:05:04 +0100

    Switch to mongodb and removed filtering logic

--- crawler.py
+++ crawler.py
@@ -1,51 +1,16 @@
 #!/usr/bin/env python3
+import datetime
 import json
 import os
-import re
 import requests
-import sqlite3
 import time
-from pathlib import Path
 from termcolor import colored
-
-connection = None
-cursor = None
-DB_FILENAME = 'scrape.db'
-OUTPUT_DIRECTORY = 'loot'
+from pymongo import MongoClient
 
 
 class PastebinCrawler:
-
-    def __init__(self):
-        self.load_regexes()
-        self.connect_db()
-
-    # def table_exists(database, table):
-    #     SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';
-
-    def connect_db(self):
-        self.connection = sqlite3.connect(DB_FILENAME)
-        self.cursor = self.connection.cursor()
-        try:
-            self.cursor.execute('SELECT * FROM scraped_bins')
-        except sqlite3.OperationalError:
-            self.log('Creating scraped_bins table', 'warning')
-            self.cursor.execute('CREATE TABLE scraped_bins (key TEXT PRIMARY KEY, '
-                        'interesting BOOLEAN NOT NULL, date TEXT NOT NULL)')
-            self.connection.commit()
-
-
     def get_timestamp(self):
         return time.strftime('%d/%m/%y %H:%M:%S')
-
-
-    def get_timestamp_as_path(self):
-        timestamp = self.get_timestamp()
-        timestamp = timestamp.replace('/', '_')
-        timestamp = timestamp.replace(':', '_')
-        timestamp = timestamp.replace(' ', '__')
-        return timestamp
-
 
     def log(self, message, type='neutral', clear_line=False):
         symbol = None
@@ -70,84 +35,75 @@ class PastebinCrawler:
 
         print('%s %s %s' % (date, symbol, message), end=line_ending)
 
+    def get_collection(self):
+        client = MongoClient()
+        db_name = self.get_db_name()
+        db = client['pastebin']
+        collection = db[db_name]
+        client.close()
+        return collection
 
-    def check_paste(self, key):
+    def get_db_name(self):
+        today = datetime.datetime.today()
+        year, month, day = today.isocalendar()
+        return f'{year}-{month}'
+
+    def save_to_db(self, paste):
+        collection = self.get_collection()
+        collection.insert_one(paste)
+
+    def get_paste(self, key):
         url = 'https://scrape.pastebin.com/api_scrape_item.php?i=' + key
-        paste_text = requests.get(url).text
-        found = False
-        for regex, file, directory in self.regexes:
-            if re.match(regex, paste_text, re.IGNORECASE):
-                self.log('Found a matching paste: %s -> %s' % (key, file), 'positive')
-                self.save_result(paste_text, url, key, file, directory)
-                found = True
-        if not found:
-            self.log(f'Paste {key} not matching', 'negative', True)
-            timestamp = self.get_timestamp_as_path()
-            path = f'{OUTPUT_DIRECTORY}/uncategorized/{timestamp}_{key}'
-            f = open(path, 'w')
-            f.write(paste_text)
-            return 0
-        return 1
+        retries = 10
+        while retries > 0:
+            r = requests.get(url)
+            if r.status_code == 200:
+                return r.text
+            retries -= 1
 
+    def db_contains(self, key):
+        collection = self.get_collection()
+        result = collection.find({'key': key})
+        return result.count() > 0
 
-    def save_result(self, paste_text, url, paste_id, file, directory):
-        timestamp = self.get_timestamp()
-        with open(f'{OUTPUT_DIRECTORY}/{file}', 'a') as matching:
-            matching.write(f'{timestamp} - {url}\n')
-        timestamp = self.get_timestamp_as_path()
-        paste_id = paste_id.replace('/', '')
-        path = f'{OUTPUT_DIRECTORY}/{directory}/{timestamp}_{paste_id}.txt'
-        with open(path, 'w') as f:
-            f.write(paste_text + '\n')
-
-
-    def insert_in_db(self, key):
-        timestamp = self.get_timestamp()
-        interesting = self.check_paste(key)
-        query_string = f'INSERT INTO scraped_bins (key, interesting, date) ' \
-                    f'VALUES ("{key}", {interesting}, "{timestamp}")'
-        self.connection.execute(query_string)
-        self.connection.commit()
-
-
-    def search_in_db(self, key):
-        query_string = f'SELECT * FROM scraped_bins WHERE key = "{key}"'
-        self.cursor.execute(query_string)
-        return len(self.cursor.fetchall()) != 0
-
-
-    def load_regexes(self):
-        f = open('regexes.txt')
-        self.regexes = []
-        lines = f.read().splitlines()
-        for line in lines:
-            if len(line) > 0 and not line.startswith('#'):
-                fields = line.split(',')
-                self.regexes.append([field.strip() for field in fields])
-
-        for i in range(len(self.regexes)):
-            self.regexes[i] = [','.join(self.regexes[i][:-2])] + self.regexes[i][-2:]
-            category = self.regexes[i][2]
-            directory = f'{OUTPUT_DIRECTORY}/{category}'
-            Path(directory).mkdir(parents=True, exist_ok=True)
-        uncategorized_path = f'{OUTPUT_DIRECTORY}/uncategorized'
-        Path(uncategorized_path).mkdir(parents=True, exist_ok=True)
-
+    def make_request(self, url, convert_json=False, retries=10):
+        while retries > 0:
+            try:
+                response = requests.get(url)
+                if response.status_code == 200:
+                    if convert_json:
+                        return json.loads(response.text)
+                    else:
+                        return response.text
+            except json.decoder.JSONDecodeError:
+                pass
+            retries -= 1
+        raise Exception(f'Could not get {url}')
 
     def scrape(self):
-        SCRAPE_URL = 'https://scrape.pastebin.com/api_scraping.php?limit=250'
+        scrape_url = 'https://scrape.pastebin.com/api_scraping.php?limit=250'
+        fields = ['date', 'key', 'expire', 'title', 'syntax', 'user',
+                  'content']
 
         while True:
-            response = requests.get(SCRAPE_URL)
-            data = json.loads(response.text)
+            data = self.make_request(scrape_url, convert_json=True)
+            count = 0
             for paste in data:
                 key = paste['key']
-                if self.search_in_db(key):
-                    self.log(f'Paste {key} already checked', 'negative', True)
-                else:
-                    self.insert_in_db(paste['key'])
+                paste_text = self.get_paste(key)
+                paste['content'] = paste_text
+                if not self.db_contains(key):
+                    count += 1
+                    filtered_paste = {}
+                    for field in fields:
+                        filtered_paste[field] = paste[field]
+                    self.save_to_db(filtered_paste)
+                    self.log(f'Saving {key}')
+            if count:
+                self.log(f'Found {count} new pastes', 'positive')
+            self.log(f'Waiting 60 seconds...')
             time.sleep(60)
-    
+
 
 def main():
     crawler = PastebinCrawler()
